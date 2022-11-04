8b0bc6e574b0272b9e0a67626eacaf1f9e972277
Jafar <jafar@iMac-Pro-de-Jafar.local>
2022-11-04 17:26:02 +0100

    fixes ios bug

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -39,219 +39,218 @@ class _InfoScreenState extends State<InfoScreen> {
                 centerTitle: true,
                 title: Text(AppLocalizations.of(context)!.medInfo),
               ),
-              body: SafeArea(
-                  child: Column(
+              body: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: Container(
-                      color: Colors.transparent,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 8,
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(35, 35, 35, 35),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20)),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
+              Expanded(
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+              Expanded(
+                flex: 8,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(35, 35, 35, 35),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Center(
-                              child: SizedBox(
-                                child: Image.asset("images/${data1['image']}"),
-                                height: 120,
-                              ),
-                            ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Center(
+                          child: SizedBox(
+                            child: Image.asset("images/${data1['image']}"),
+                            height: 120,
                           ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Center(
-                                    child: Text(
-                                      data1['name']+" (${data1['dosage']})",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 22),
-                                    ),
-                                  ),
-                                  Text(
-                                    data1['note'],
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Center(
+                                child: Text(
+                                  data1['name']+" (${data1['dosage']})",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22),
+                                ),
                               ),
-                            ),
+                              Text(
+                                data1['note'],
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
                           ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
                               children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    InfoContainer(
-                                      title:
-                                          AppLocalizations.of(context)!.start,
-                                      info: data1['dates'][0],
-                                    ),
-                                    InfoContainer(
-                                      title: AppLocalizations.of(context)!.end,
-                                      info: data1['dates'].last,
-                                    )
-                                  ],
+                                InfoContainer(
+                                  title:
+                                      AppLocalizations.of(context)!.start,
+                                  info: data1['dates'][0],
                                 ),
-                                SizedBox(
-                                  height: 30,
+                                InfoContainer(
+                                  title: AppLocalizations.of(context)!.end,
+                                  info: data1['dates'].last,
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                              children: [
+                                InfoContainer(
+                                  title: AppLocalizations.of(context)!.time,
+                                  info: data1['time'],
                                 ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    InfoContainer(
-                                      title: AppLocalizations.of(context)!.time,
-                                      info: data1['time'],
-                                    ),
-                                    InfoContainer(
-                                      title: AppLocalizations.of(context)!.freq,
-                                      info: data1['frequency'],
-                                    )
-                                  ],
+                                InfoContainer(
+                                  title: AppLocalizations.of(context)!.freq,
+                                  info: data1['frequency'],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Center(
+                        child: GestureDetector(
+                          onTap: () async {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      EditScreen(doc: widget.doc)),
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(
+                                      0, 3), // changes position of shadow
                                 ),
                               ],
                             ),
-                          ),
-                          Center(
-                            child: GestureDetector(
-                              onTap: () async {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          EditScreen(doc: widget.doc)),
-                                );
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.5),
-                                      spreadRadius: 5,
-                                      blurRadius: 7,
-                                      offset: Offset(
-                                          0, 3), // changes position of shadow
-                                    ),
-                                  ],
+                            height: 50,
+                            width: 190,
+                            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
                                 ),
-                                height: 50,
-                                width: 190,
-                                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Icon(
-                                        Icons.edit,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)!.edit,
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20),
-                                    ),
-                                    Spacer(),
-                                  ],
+                                Text(
+                                  AppLocalizations.of(context)!.edit,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
                                 ),
-                              ),
+                                Spacer(),
+                              ],
                             ),
                           ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Center(
-                            child: GestureDetector(
-                              onTap: () async {
-                                List id = data1['id'];
-                                for (var i = 0; i < id.length; i++) {
-                                  await flutterLocalNotificationsPlugin
-                                      .cancel(id[i]);
-                                  print(id[i]);
-                                }
-                                await FirebaseFirestore.instance
-                                    .collection(userid)
-                                    .doc(widget.doc)
-                                    .delete();
-                                Navigator.pop(context);
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(5)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.5),
-                                      spreadRadius: 5,
-                                      blurRadius: 7,
-                                      offset: Offset(
-                                          0, 3), // changes position of shadow
-                                    ),
-                                  ],
-                                ),
-                                height: 50,
-                                width: 190,
-                                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Icon(
-                                        Icons.delete,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)!.del,
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20),
-                                    ),
-                                    Spacer(),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
-                  )
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: GestureDetector(
+                          onTap: () async {
+                            List id = data1['id'];
+                            for (var i = 0; i < id.length; i++) {
+                              await flutterLocalNotificationsPlugin
+                                  .cancel(id[i]);
+                              print(id[i]);
+                            }
+                            await FirebaseFirestore.instance
+                                .collection(userid)
+                                .doc(widget.doc)
+                                .delete();
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(
+                                      0, 3), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            height: 50,
+                            width: 190,
+                            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  AppLocalizations.of(context)!.del,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                Spacer(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
                 ],
-              )),
+              ),
             );
           }
           return Container();
